07ce5d459eea6b9443a8d6a3808b26d7f4d3eb24
walljm <walljm@gmail.com>
2019-08-01 17:20:28 -0400

    formatting and updates to ops command

--- profile.ps1
+++ profile.ps1
@@ -2,7 +2,8 @@
 $projects = "c:\projects"
 
 
-function prompt {
+function prompt
+{
     $origLastExitCode = $LASTEXITCODE
 
     $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
@@ -12,162 +13,172 @@ function prompt {
     "$('>' * ($nestedPromptLevel + 1)) "
 }
 
-function cmds {
+function cmds
+{
     echo ""
     echo " Misc"
     echo " ----------------------------------------------------------------"
     echo "   grep == sls $args"
     echo "    nst == netstat -n -b"
-	echo "    cdp == cd  $projects"
-	echo "   cdop == $projects\vae\ops\packaging"
-	echo "     dc == docker-compose $args"
-	echo "    dco == dc -f docker-compose.yml -f docker-compose.local.yml"
-	echo "     rn == react-native $args"
-	echo "     io == ionic $args"
-	echo "    ioc == ionic cordova $args"
-	echo "    dsh == docker exec -it $args /bin/bash"
-	echo "   hist == Get-History"
+    echo "    cdp == cd  $projects"
+    echo "   cdop == $projects\vae\ops\packaging"
+    echo "     dc == docker-compose $args"
+    echo "    dco == dc -f docker-compose.yml -f docker-compose.local.yml"
+    echo "     rn == react-native $args"
+    echo "     io == ionic $args"
+    echo "    ioc == ionic cordova $args"
+    echo "    dsh == docker exec -it $args /bin/bash"
+    echo "   hist == Get-History"
     echo ""
-	echo "  ---------------------------------------------------------------"
-	echo "  git aliases"
-	echo "  ---------------------------------------------------------------"
-	echo "   gt update $branch =>"
-	echo "      git checkout $other --force"
-	echo "		git clean -f -d -x"
-	echo "		git reset --hard"
-	echo ""
-	echo "   gt heads =>"
-	echo "      git branch"
-	echo ""
-	echo "   gt clean =>"
-	echo "		git clean -f -d -x"
-	echo "		git reset --hard"
-	echo ""
-	echo "   gt rebase $t1 $t2 =>"
-	echo "		git checkout $t1"
-	echo "		git rebase $t2"
-	echo ""
-	echo "-----------------------------------------------------------------"
-	echo ""
-	echo ""
+    echo "  ---------------------------------------------------------------"
+    echo "  git aliases"
+    echo "  ---------------------------------------------------------------"
+    echo "   gt update $branch =>"
+    echo "      git checkout $other --force"
+    echo "      git clean -f -d -x"
+    echo "      git reset --hard"
+    echo ""
+    echo "   gt heads =>"
+    echo "      git branch"
+    echo ""
+    echo "   gt clean =>"
+    echo "		git clean -f -d -x"
+    echo "		git reset --hard"
+    echo ""
+    echo "   gt rebase $t1 $t2 =>"
+    echo "		git checkout $t1"
+    echo "		git rebase $t2"
+    echo ""
+    echo "-----------------------------------------------------------------"
+    echo ""
+    echo ""
 }
 
-function hist { 
+function hist
+{ 
     get-history
 }
 
-function grep { 
-    sls $args
+function grep
+{ 
+    Select-String $args
 }
 
-function nst {
+function nst
+{
     echo "netstat -n $($args -join ' ')"
     netstat -n $args
 }
 
-function cdp {
-	param (
-        [Parameter(Mandatory=$false)]
+function cdp
+{
+    param (
+        [Parameter(Mandatory = $false)]
         [ArgumentCompleter( {
-            param ( $commandName,
+                param ( $commandName,
                     $parameterName,
                     $wordToComplete,
                     $commandAst,
                     $fakeBoundParameters )
-			
-			$dir = "$projects\$wordToComplete"			
-			if ($wordToComplete -eq "")
-			{
-				$dir = "$projects\a"
-			}
-			if ($wordToComplete.EndsWith("\"))
-			{
-				$dir = $dir + "a"
-			}
-			$examine = split-path -path $dir
-			$pre = $examine.Substring($projects.length)
-			if ($pre.length -gt 0)
-			{
-				$pre = "$pre\"
-			}
-			if ($pre.StartsWith("\"))
-			{
-				$pre = $pre.Substring(1)
-			}
-			$test = $wordToComplete.split('\') | select -last 1
-			Get-ChildItem $examine | ?{ $_.PSIsContainer } | select Name | where {$_ -like "*$test*"} | Foreach {"$($pre)$($_.Name)"}
+            
+                $dir = "$projects\$wordToComplete"			
+                if ($wordToComplete -eq "")
+                {
+                    $dir = "$projects\a"
+                }
+                if ($wordToComplete.EndsWith("\"))
+                {
+                    $dir = $dir + "a"
+                }
+                $examine = split-path -path $dir
+                $pre = $examine.Substring($projects.length)
+                if ($pre.length -gt 0)
+                {
+                    $pre = "$pre\"
+                }
+                if ($pre.StartsWith("\"))
+                {
+                    $pre = $pre.Substring(1)
+                }
+                $test = $wordToComplete.split('\') | Select-Object -last 1
+            Get-ChildItem $examine | Where-Object { $_.PSIsContainer } | Select-Object Name | Where-Object { $_ -like "*$test*" } | ForEach-Object { "$($pre)$($_.Name)" }
 
-        } )]
-        $args
-      )
-	echo $args
-	if ($args)
-	{
-		echo "cd  $projects\$args"
-		cd $projects\$args
-	}
-	else
-	{
-		echo "cd  $projects"
-		cd  $projects
-	}
+    } )]
+$args
+)
+if ($args)
+{
+    echo "cd  $projects\$args"
+    cd $projects\$args
+}
+else
+{
+    echo "cd  $projects"
+    cd  $projects
+}
 }
 
-function dc {
-	echo "docker-compose $($args -join ' ')"
-	docker-compose $args
+function dc
+{
+    echo "docker-compose $($args -join ' ')"
+    docker-compose $args
 }
 
-function rn {
-	echo "react-native $($args -join ' ')"
-	react-native $args
+function rn
+{
+    echo "react-native $($args -join ' ')"
+    react-native $args
 }
 
-function io {
-	echo "ionic $($args -join ' ')"
-	ionic $args
+function io
+{
+    echo "ionic $($args -join ' ')"
+    ionic $args
 }
 
-function ioc {
-	echo "ionic cordova $($args -join ' ')"
-	ionic cordova $args
+function ioc
+{
+    echo "ionic cordova $($args -join ' ')"
+    ionic cordova $args
 }
 
-function dsh {
-	echo "docker exec -it $($args -join ' ') /bin/bash"
-	docker exec -it $args /bin/bash 
+function dsh
+{
+    echo "docker exec -it $($args -join ' ') /bin/bash"
+    docker exec -it $args /bin/bash 
 }
 
 
-function gt {
-	$cmd, $other = $args
-	
-	if ($cmd -eq "update")
-	{
-		echo "git checkout $other --force"
-		git checkout $other --force
-		git clean -f -d -x
-		git reset --hard
-	}
-	elseif ($cmd -eq "heads")
-	{
-		git branch
-	}
-	elseif ($cmd -eq "clean")
-	{
-		git clean -f -d -x
-		git reset --hard
-	}
-	elseif ($cmd -eq "rebase")
-	{
-	    $t1, $t2 = $other
-		git checkout $t1
-		git rebase $t2
-	}
-	else
-	{
-		git $cmd $other
-	}
+function gt
+{
+    $cmd, $other = $args
+    
+    if ($cmd -eq "update")
+    {
+        echo "git checkout $other --force"
+        git checkout $other --force
+        git clean -f -d -x
+        git reset --hard
+    }
+    elseif ($cmd -eq "heads")
+    {
+        git branch
+    }
+    elseif ($cmd -eq "clean")
+    {
+        git clean -f -d -x
+        git reset --hard
+    }
+    elseif ($cmd -eq "rebase")
+    {
+        $t1, $t2 = $other
+        git checkout $t1
+        git rebase $t2
+    }
+    else
+    {
+        git $cmd $other
+    }
 }
 
 echo "-----------------------------------------------------------------"
--- vae.ps1
+++ vae.ps1
@@ -1,185 +1,202 @@
 $opsfolder = "$projects\vae\ops"
-function vcmds { 
+function vcmds
+{ 
     echo ""
     echo " VAE "
     echo " ----------------------------------------------------------------"
-	echo "   cdpo == $opsfolder\packaging"
-	echo "    dco == dc -f docker-compose.yml -f docker-compose.local.yml"
-	echo "    ops == demo|test|dev clean|pull"
+    echo "   cdpo == $opsfolder\packaging"
+    echo "    dco == dc -f docker-compose.yml -f docker-compose.local.yml"
+    echo "    ops == demo|test|dev clean|pull"
 }
 
-function cdpo {
-	param (
-        [Parameter(Mandatory=$false)]
+function cdpo
+{
+    param (
+        [Parameter(Mandatory = $false,
+            HelpMessage = "If used, dumps you into the demo packing folder")]
+        [Alias("demo")]
+        [Switch]
+        $d,
+        [Parameter(Mandatory = $false,
+            HelpMessage = "If used, dumps you into the test packing folder")]
+        [Alias("test")]
+        [Switch]
+        $t,
+        [Parameter(Mandatory = $false)]
         [ArgumentCompleter( {
-            param ( $commandName,
+                param ( $commandName,
                     $parameterName,
                     $wordToComplete,
                     $commandAst,
                     $fakeBoundParameters )
-			
-			$dir = "$opsfolder\$wordToComplete"			
-			if ($wordToComplete -eq "")
-			{
-				$dir = "$opsfolder\a"
-			}
-			if ($wordToComplete.EndsWith("\"))
-			{
-				$dir = $dir + "a"
-			}
-			$examine = split-path -path $dir
-			$pre = $examine.Substring($opsfolder.length)
-			if ($pre.length -gt 0)
-			{
-				$pre = "$pre\"
-			}
-			if ($pre.StartsWith("\"))
-			{
-				$pre = $pre.Substring(1)
-			}
-			$test = $wordToComplete.split('\') | select -last 1
-			Get-ChildItem $examine | ?{ $_.PSIsContainer } | select Name | where {$_ -like "*$test*"} | Foreach {"$($pre)$($_.Name)"}
+            
+                $dir = "$opsfolder\$wordToComplete"			
+                if ($wordToComplete -eq "")
+                {
+                    $dir = "$opsfolder\a"
+                }
+                if ($wordToComplete.EndsWith("\"))
+                {
+                    $dir = $dir + "a"
+                }
+                $examine = split-path -path $dir
+                $pre = $examine.Substring($opsfolder.length)
+                if ($pre.length -gt 0)
+                {
+                    $pre = "$pre\"
+                }
+                if ($pre.StartsWith("\"))
+                {
+                    $pre = $pre.Substring(1)
+                }
+                $test = $wordToComplete.split('\') | Select-Object -last 1
+            Get-ChildItem $examine | Where-Object { $_.PSIsContainer } | Select-Object Name | Where-Object { $_ -like "*$test*" } | ForEach-Object { "$($pre)$($_.Name)" }
 
-        } )]
-        $args
-      )
-	echo $args
-	echo $args
-	if ($args)
-	{
-		echo "cd $opsfolder\$args"
-		cd $opsfolder\$args
-	}
-	else
-	{
-		echo "cd $opsfolder\packaging"
-		cd  $opsfolder\packaging
-	}
+    } )]
+$args
+)
+if ($args)
+{
+    echo "cd $opsfolder\$args"
+    cd $opsfolder\$args
 }
-function dco {
- docker-compose -f docker-compose.yml -f docker-compose.local.yml $args
+else
+{
+    echo "cd $opsfolder\packaging"
+    cd  $opsfolder\packaging
 }
-
-function gto {
-    #Set-PSDebug -Trace 1
-	echo "git $($args -join ' ')"
-	$dirs = get-childitem $opsfolder -directory
-
-	foreach ($dir in $dirs) {
-		cd $opsfolder	
-		cd $dir.Name
-		pwd
-		echo "git $($args -join ' ')"
-		gt $args
-		echo ""
-		cd ..
-	}
-
-	#Set-PSDebug -Off
+}
+function dco
+{
+    docker-compose -f docker-compose.yml -f docker-compose.local.yml $args
 }
 
-function ops {
-     param(
-         [Parameter(Mandatory=$false,
-			HelpMessage="If used, does a 'pull' before bring the system up.")]
-		 [Alias("pull")]
-		 [Switch]
-         $p,
-         [Parameter(Mandatory=$false,
-			HelpMessage="If used, does a 'down -v' before bring the system up.")]
-		 [Alias("clean")]
-		 [Switch]
-         $c,
-		 [String]
-		 [Parameter(Mandatory=$true, Position=0,
-			HelpMessage="Enter one of the following: test, demo, dev")]
-		 [ValidateSet("test", "demo", "dev")]
-		 $cmd)
+function ops
+{
+    param(
+        [Parameter(Mandatory = $false,
+            HelpMessage = "If used, does a 'pull' before bring the system up.")]
+        [Alias("pull")]
+        [Switch]
+        $p,
+        [Parameter(Mandatory = $false,
+            HelpMessage = "If used, does a 'down -v' before bring the system up.")]
+        [Alias("clean")]
+        [Switch]
+        $c,
+        [Parameter(Mandatory = $false,
+            HelpMessage = "If used, stops the webclient and itpie-api containers, so you can run them from source.")]
+        [Alias("headless")]
+        [Switch]
+        $h,
+        [Parameter(Mandatory = $false,
+            HelpMessage = "If used, stops all services using docker-compose down.")]
+        [Alias("down")]
+        [Switch]
+        $d,
+        [String]
+        [Parameter(Mandatory = $false, Position = 0,
+            HelpMessage = "Enter one of the following: test, demo, dev")]
+        [ValidateSet("test", "demo", "dev", "")]
+        $cmd)
+    if ($cmd -eq "")
+    {
+        echo ""
+        echo "Syntax: ops `$cmd -p|pull -c|clean -h|headless"
+        echo ""
+        echo " `$cmd         - one of the following: 'test', 'demo', 'dev'"
+        echo " -p|pull      - If used, does a 'pull' before bring the system up."
+        echo " -c|clean     - If used, does a 'down -v' before bring the system up."
+        echo " -h|headless  - If used, stops the webclient and itpie-api containers, so you can run them from source."
+        return
+    }
 	
-	if ($cmd -eq "demo")
-	{
-		echo ""
-		echo "Stopping OPS dev..."
-		echo ""
-		cd "$projects\vae\ops\packaging"
-		dco down
-		
-		echo ""
-		echo "Stopping OPS test..."
-		echo ""
-		cd "$projects\vae\test"
-		dco down
-		
-		echo ""
-		echo "Starting OPS demo..."
-		echo ""
-		cd "$projects\vae\demo"
-		if ($c)
-		{
-			dco down -v
-		}
-		if ($p)
-		{
-			dco pull
-		}
-		dco up -d
-	}
-	elseif ($cmd -eq "test")
-	{
-		echo ""
-		echo "Stopping OPS dev..."
-		echo ""
-		cd "$projects\vae\ops\packaging"
-		dco down
-		
-		echo ""
-		echo "Stopping OPS demo..."
-		echo ""
-		cd "$projects\vae\demo"
-		dco down
-		
-		echo ""
-		echo "Starting OPS test..."
-		echo ""
-		cd "$projects\vae\test"
-		if ($c)
-		{
-			dco down -v
-		}
-		if ($p)
-		{
-			dco pull
-		}
-		dco up -d
-	}
-	elseif ($cmd -eq "dev")
-	{
-		echo ""
-		echo "Stopping OPS test..."
-		echo ""
-		cd "$projects\vae\test"
-		dco down
-		
-		echo ""
-		echo "Stopping OPS demo..."
-		echo ""
-		cd "$projects\vae\demo"
-		dco down
-		
-		echo ""
-		echo "Starting OPS dev..."
-		echo ""
-		cd "$projects\vae\ops\packaging"
-		if ($c)
-		{
-			dco down -v
-		}
-		if ($p)
-		{
-			dco pull
-		}
-		dco up -d
-	}
+    if ($cmd -eq "demo")
+    {
+        echo ""
+        echo "Stopping OPS dev..."
+        echo ""
+        cd "$projects\vae\ops\packaging"
+        dco down
+        
+        echo ""
+        echo "Stopping OPS test..."
+        echo ""
+        cd "$projects\vae\test"
+        dco down
+        
+        cd "$projects\vae\demo"
+    }
+    elseif ($cmd -eq "test")
+    {
+        echo ""
+        echo "Stopping OPS dev..."
+        echo ""
+        cd "$projects\vae\ops\packaging"
+        dco down
+        
+        echo ""
+        echo "Stopping OPS demo..."
+        echo ""
+        cd "$projects\vae\demo"
+        dco down
+        
+        cd "$projects\vae\test"
+    }
+    elseif ($cmd -eq "dev")
+    {
+        echo ""
+        echo "Stopping OPS test..."
+        echo ""
+        cd "$projects\vae\test"
+        dco down
+        
+        echo ""
+        echo "Stopping OPS demo..."
+        echo ""
+        cd "$projects\vae\demo"
+        dco down
+        
+        cd "$projects\vae\ops\packaging"
+    }
+    
+        
+    if ($c)
+    {
+        echo ""
+        echo "Cleaning OPS $cmd..."
+        echo ""
+        dco down -v
+    }
+
+    if ($p)
+    {
+        echo ""
+        echo "Pulling OPS $cmd..."
+        echo ""
+        dco pull
+    }
+
+    if ($d)
+    {
+        echo ""
+        echo "Stopping OPS $cmd..."
+        echo ""
+        dco down
+    }
+    else 
+    {
+        echo ""
+        echo "Starting OPS $cmd..."
+        echo ""
+        dco up -d
+
+        if ($h)
+        {
+            dco stop itpie-api
+            dco stop webclient
+        }
+    }
 }
 
 vcmds
